170307e2e761ce36a82b30de9a769de1560e285f
Oliver Foggin <oliverfoggin@me.com>
2021-11-06 14:18:48 +0000

    Initial Commit

---
+++ SpaceX-Vanilla/SpaceX_VanillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_VanillaApp.swift
+//  SpaceX-Vanilla
+//
+//  Created by Oliver Foggin on 06/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_VanillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
